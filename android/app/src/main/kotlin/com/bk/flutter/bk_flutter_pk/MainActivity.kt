1c166a3436542e68f4956c1baa4e847a271406ae
xiekongying001 <xiekongying001@ke.com>
2021-12-01 16:35:18 +0800

    项目初始化

---
+++ android/app/src/main/kotlin/com/bk/flutter/bk_flutter_pk/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bk.flutter.bk_flutter_pk
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/xiekongying/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/xiekongying/Desktop/xky/learn/flutter/pk/bk_flutter_pk/bk_flutter_pk"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/xiekongying/Library/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
